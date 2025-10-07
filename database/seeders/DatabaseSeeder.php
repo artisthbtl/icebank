17d5a3c8a442a8e36b1b7ec87db47543c149136e
artisthbtl <eyokusumo@gmail.com>
2025-10-07 15:57:49 +0700

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // --- SCENARIO 1: CREATE A MAIN "POWER USER" FOR EASY TESTING ---
-        // This user will have a rich history of transactions and subscriptions.
+        // CREATE A MAIN "POWER USER"
         $powerUser = User::factory()
             ->has(Account::factory(['balance' => 50000])) // Create a user with one account that has a high balance
             ->create([
@@ -25,52 +24,41 @@ class DatabaseSeeder extends Seeder
                 'email' => 'marcello@icebank.test',
             ]);
 
-        // --- SCENARIO 2: CREATE A BATCH OF RANDOM USERS ---
-        // These users will be recipients for transfers and have their own simple histories.
+        // CREATE A BATCH OF RANDOM USERS
         $randomUsers = User::factory()
             ->count(20)
             ->has(Account::factory()) // Every user gets one account
             ->create();
 
-        // --- SCENARIO 3: CREATE COMPANIES, SERVICES, AND PLANS ---
-        // We'll create a few companies, each with its own set of services and plans.
+        // CREATE COMPANIES, SERVICES, AND PLANS
         $netflix = Company::factory()->create(['name' => 'Netflix']);
         $spotify = Company::factory()->create(['name' => 'Spotify']);
         $gym = Company::factory()->create(['name' => 'IceGym']);
 
-        // Create a service and a plan for Netflix
         $netflixService = Service::factory()->for($netflix)->create(['name' => 'Video Streaming', 'type' => 'Entertainment']);
         $netflixPlan = Plan::factory()->for($netflixService)->create(['price' => 15.99, 'duration' => 30]);
 
-        // Create a service and multiple plans for Spotify
         $spotifyService = Service::factory()->for($spotify)->create(['name' => 'Music Streaming', 'type' => 'Entertainment']);
         Plan::factory()->for($spotifyService)->create(['price' => 9.99, 'duration' => 30]);
         Plan::factory()->for($spotifyService)->create(['price' => 99.99, 'duration' => 365]);
 
-        // Create multiple services and plans for the Gym
         $gymService = Service::factory()->for($gym)->create(['name' => 'Gym Membership', 'type' => 'Lifestyle']);
         $yogaService = Service::factory()->for($gym)->create(['name' => 'Yoga Classes', 'type' => 'Lifestyle']);
         $gymPlan = Plan::factory()->for($gymService)->create(['price' => 45.00, 'duration' => 30]);
         Plan::factory()->for($yogaService)->create(['price' => 60.00, 'duration' => 30]);
 
 
-        // --- SCENARIO 4: POPULATE THE POWER USER'S TRANSACTION HISTORY ---
-
-        // Give the power user several "add_balance" transactions
+        // POPULATE THE POWER USER'S TRANSACTION HISTORY
         Transaction::factory()->count(5)->for($powerUser->account)->create();
 
-        // Create several "transfer" transactions FROM the power user TO random users
         Transaction::factory()
             ->count(3)
             ->for($powerUser->account)
             ->transfer()
             ->create([
-                // Override the factory to pick one of our existing random users as the recipient
                 'related_account_id' => $randomUsers->random()->account->id,
             ]);
 
-        // Create several subscriptions for the power user. The SubscriptionFactory will
-        // automatically create the 'pay_plan' transaction behind the scenes.
         Subscription::factory()->create([
             'user_id' => $powerUser->id,
             'plan_id' => $netflixPlan->id,
@@ -81,14 +69,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
-        // --- SCENARIO 5: GIVE RANDOM USERS SOME ACTIVITY ---
-        
-        // Let's make some of the random users have a few transactions as well
+        // GIVE RANDOM USERS SOME ACTIVITY
         foreach ($randomUsers->take(5) as $user) {
-            // Give them a couple of balance top-ups
             Transaction::factory()->count(2)->for($user->account)->create();
 
-            // Give them one subscription
             Subscription::factory()->create(['user_id' => $user->id]);
         }
     }
