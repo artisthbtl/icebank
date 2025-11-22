b8944dc94f3720c00c4631fcb447867a79d73184
artisthbtl <eyokusumo@gmail.com>
2025-11-22 19:08:48 +0700

    minor change on the head icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link 
+        rel="icon" 
+        type="image/svg+xml" 
+        href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%233B82F6' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'%3E%3C/path%3E%3Cline x1='3.27' y1='6.96' x2='12' y2='12.01'%3E%3C/line%3E%3Cline x1='12' y1='22.08' x2='12' y2='12'%3E%3C/line%3E%3Cline x1='20.73' y1='6.96' x2='12' y2='12'%3E%3C/line%3E%3C/svg%3E"
+    >
     <title>Icebank</title>
     @routes
     @viteReactRefresh
